adb1a408134cfa40e0fb78ed9c01c3e6ec8cfe5f
gnmmdk <345498912@qq.com>
2019-12-05 20:07:31 +0800

    b u

--- app/src/main/java/com/kangjj/custom/okhttp/MainActivity.java
+++ app/src/main/java/com/kangjj/custom/okhttp/MainActivity.java
@@ -22,8 +22,8 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-    private final String PATH = "http://restapi.amap.com/v3/weather/weatherInfo?city=110101&key=13cb58f5884f9749287abbead9c658f2";
-//    private final String PATH = "http://restapi.amap.com/v3/weather/weatherInfo";
+//    private final String PATH = "http://restapi.amap.com/v3/weather/weatherInfo?city=110101&key=13cb58f5884f9749287abbead9c658f2";
+    private final String PATH = "http://restapi.amap.com/v3/weather/weatherInfo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,11 +61,11 @@ public class MainActivity extends AppCompatActivity {
     public void useMyOkhttp(View view) {
         OkHttpClient2 okHttpClient2 = new OkHttpClient2.Builder().build();
 
-//        RequestBody2 requestBody = new RequestBody2();
-//        requestBody.addBody("city","110101");
-//        requestBody.addBody("key","13cb58f5884f9749287abbead9c658f2");
+        RequestBody2 requestBody = new RequestBody2();
+        requestBody.addBody("city","110101");
+        requestBody.addBody("key","13cb58f5884f9749287abbead9c658f2");
 
-        Request2 request2 = new Request2.Builder()/*.post(requestBody)*/.url(PATH).build();
+        Request2 request2 = new Request2.Builder().post(requestBody).url(PATH).build();
 
         Call2 call2 = okHttpClient2.newCall(request2);
 
